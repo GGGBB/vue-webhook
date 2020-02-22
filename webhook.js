6b0418f2c96cf20d847d78d88909d378af54daab
gggbb <wangzifeng2008@qq.com>
2020-02-22 19:35:37 +0800

    add logs date

--- webhook.js
+++ webhook.js
@@ -2,6 +2,27 @@ let http = require('http')
 let crypto = require('crypto')
 let {spawn} = require('child_process')
 let SECRET = '123456'
+/**
+* 扩展Date的Format函数
+* 月(M)、日(d)、小时(h)、分(m)、秒(s)、季度(q) 可以用 1-2 个占位符， 
+* 年(y)可以用 1-4 个占位符，毫秒(S)只能用 1 个占位符(是 1-3 位的数字) 
+* @param {[type]} fmt [description]
+*/
+Date.prototype.Format = function(fmt) { //author: meizz 
+   var o = {
+       "M+": this.getMonth() + 1, //月份 
+       "d+": this.getDate(), //日 
+       "h+": this.getHours(), //小时 
+       "m+": this.getMinutes(), //分 
+       "s+": this.getSeconds(), //秒 
+       "S": this.getMilliseconds() //毫秒 
+   };
+   if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+   for (var k in o)
+       if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+   return fmt;
+}
+
 function sign(body){
   return 'sha1='+ crypto.createHmac('sha1', SECRET).update(body).digest('hex')
 }
@@ -26,13 +47,13 @@ let server = http.createServer(function(req, res){
         let payload = JSON.parse(body)
         let child = spawn('sh', [`./${payload.repository.name}.sh`])
         child.stdout.on('data', function(data){
-          console.log('stdout---y--y--y--->' + data)
+          console.log((new Date()).Format("yyyy-MM-dd hh:mm:ss") + '--stdout---y--y--y--->' + data)
         })
         child.stderr.on('data', function(data){
-          console.log('stderr---x--x--x--->' + data)
+          console.log((new Date()).Format("yyyy-MM-dd hh:mm:ss") + '--stderr---x--x--x--->' + data)
         })
         child.on('exit', function(data){
-          console.log('child process exited with code---------->' + data)
+          console.log((new Date()).Format("yyyy-MM-dd hh:mm:ss") + '--child process exited with code---------->' + data)
         })
       }
     })
